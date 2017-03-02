da93b997eb2fe136e25b93ca44ddcbdebef779af
Juan Carniglia <juan.carniglia@gmail.com>
2017-03-02 17:15:06 -0300

    - 6.0.0-alpha1 version

---
+++ releases/6.0.0-alpha1/index.js
@@ -0,0 +1,11 @@
+export default function (kibana) {
+  return new kibana.Plugin({
+    name: 'kbn_sankey_vis',
+    require: ['kibana', 'elasticsearch'],
+    uiExports: {
+      visTypes: [
+        'plugins/kbn_sankey_vis/kbn_sankey_vis'
+      ]
+    }
+  });
+}
